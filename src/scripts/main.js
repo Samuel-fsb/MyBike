3308fa0b7486e62a6e60a7aa0bc87d989c2388e3
Samuel <samuel.fsbs@gmail.com>
2026-07-09 16:57:45 -0300

    Adding a hover on the element with the phone-call class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,6 @@
 const burger = document.querySelector('.header__menu__icons__burger');
 const menuLateralOpen = document.querySelector('.menu-lateral');
 const close = document.querySelector('.menu-lateral__icons__close');
-const phone = document.querySelector('.header__menu__icons__phone-number');
 
 // Botões para abrir e fechar o menu lateral.
 burger.addEventListener('click', () => {
@@ -13,9 +12,4 @@ burger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   menuLateralOpen.classList.toggle('menu-lateral--open');
-});
-
-// Botão para mostrar/esconder o telefone
-phone.addEventListener('click', () => {
-  phone.classList.toggle('header__menu__icons__phone-number--show');
 });
