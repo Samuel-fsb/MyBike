d0b1f9eb0eafcee0e6fa4790493c281ce6eda885
Samuel <samuel.fsbs@gmail.com>
2026-07-18 11:26:19 -0300

    Disabling the scroll page when the menu lateral is open.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,14 @@
 
 // Selecioando elementos HTML.
 const burger = document.querySelector('.header__menu__icons__burger');
-const menuLateralOpen = document.querySelector('.menu-lateral');
-const close = document.querySelector('.menu-lateral__icons__close');
+const menuLateralOpen = document.querySelector('.menu');
+const close = document.querySelector('.menu__icons__close');
 
 // Botões para abrir e fechar o menu lateral.
 burger.addEventListener('click', () => {
-  menuLateralOpen.classList.toggle('menu-lateral--open');
+  menuLateralOpen.classList.toggle('menu--open');
 });
 
 close.addEventListener('click', () => {
-  menuLateralOpen.classList.toggle('menu-lateral--open');
+  menuLateralOpen.classList.toggle('menu--open');
 });
