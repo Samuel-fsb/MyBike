0882f0f7ba845c3bf4ec124bc796cd9675bdac14
Samuel <samuel.fsbs@gmail.com>
2026-07-19 18:37:30 -0300

    Fixing the layout bug.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Selecioando elementos HTML.
-const burger = document.querySelector('.header__menu__icons__burger');
+const burger = document.querySelector('.header__burger');
 const menuLateralOpen = document.querySelector('.menu');
-const close = document.querySelector('.menu__icons__close');
+const close = document.querySelector('.menu__close');
 
 // Botões para abrir e fechar o menu lateral.
 burger.addEventListener('click', () => {
