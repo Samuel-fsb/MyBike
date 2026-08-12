5eacec38974be5660dd8da9fb9050f28cd1608dc
Samuel <samuel.fsbs@gmail.com>
2026-08-12 15:43:22 -0300

    Adding ID to the sidebar links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@
 const burger = document.querySelector('.header__burger');
 const menuLateralOpen = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
+const menuLinks = document.querySelectorAll('.menu__items');
+const sidebar = document.querySelector('.menu');
 
 // Botões para abrir e fechar o menu lateral.
 burger.addEventListener('click', () => {
@@ -12,4 +14,11 @@ burger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   menuLateralOpen.classList.toggle('menu--open');
+});
+
+// Closing the side menu when user clicks on link.
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    sidebar.classList.remove('menu--open');
+  });
 });
